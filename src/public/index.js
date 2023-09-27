0d9485c58cb80c99ebca641ac5d27298750fbde4
Shxuuer <a2711492759@163.com>
2023-09-28 00:21:32 +0800

    remake(download): remake download logic

--- src/public/index.js
+++ src/public/index.js
@@ -1,3 +1,5 @@
+import axios from '@/axios/index.js'
+
 export function parseMemorySize (size) {
   const list = ['B', 'KB', 'MB', 'GB', 'TB']
   let count = 0
@@ -9,4 +11,44 @@ export function parseMemorySize (size) {
     return '0B'
   }
   return size.toFixed(2) + list[count]
+}
+
+export async function solveDownloadList (info) {
+  async function solveFolder (folder, base) {
+    return new Promise(resolve => {
+      const list = []
+      axios.get('/file', {
+        params: {
+          filePath: base + folder
+        }
+      }).then(async res => {
+        for (const file of res.data) {
+          if (file.fileName.isFolder) {
+            await solveFolder(folder + file.fileName.name + '/', base).then(res => {
+              list.push(...res)
+            })
+          } else {
+            list.push(folder + file.fileName.name)
+          }
+        }
+        resolve(list)
+      })
+    })
+  }
+
+  const list = []
+  for (const file of info.files) {
+    if (file.isFolder) {
+      await solveFolder(file.name + '/', info.filePath).then(res => {
+        list.push(...res)
+      })
+    } else {
+      list.push(file.name)
+    }
+  }
+
+  return {
+    filePath: info.filePath,
+    files: list
+  }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
 import { createStore } from 'vuex'
 import axios from '@/axios/index.js'
-import { parseMemorySize } from '@/public/index.js'
+import { parseMemorySize, solveDownloadList } from '@/public/index.js'
 import { saveAs } from 'file-saver'
 import show from '@/components/notification.js'
+import JSZip from 'jszip'
 
 export default createStore({
   state () {
@@ -22,9 +23,13 @@ export default createStore({
       state.ua = ua
     },
     async downloadFile (state, info) {
-      const fileName = info.files[0].name + (info.files.length > 1 ? ' 等' + info.files.length + '个文件' : '')
+      await solveDownloadList(info).then(res => {
+        info = res
+      })
+      const fileName = (info.files.length > 1 ? `1 / ${info.files.length}:` : '') + info.files[0]
+      show('提示', `${info.files.length > 1 ? `文件包共${info.files.length}个文件` : `${info.files[0]}`} 开始下载`)
       state.downloadInfo.downloadList.push({
-        fileName: '压缩文件中',
+        fileName,
         fileSize: '0B',
         downloadedSize: '0B',
         downloadSpeed: '0B/s',
@@ -32,36 +37,49 @@ export default createStore({
         axiosController: new AbortController(),
         status: 'downloading'
       })
-      show('提示', `${fileName} 开始下载`)
+
+      const blobList = []
       const currentDownloadInfo = state.downloadInfo.downloadList[state.downloadInfo.downloadList.length - 1]
-      await axios({
-        url: '/download',
-        method: 'post',
-        data: info,
-        responseType: 'blob',
-        onDownloadProgress: progressEvent => {
-          currentDownloadInfo.fileName = fileName
-          currentDownloadInfo.fileSize = parseMemorySize(progressEvent.total)
-          currentDownloadInfo.downloadPercent = Number((progressEvent.loaded / progressEvent.total * 100).toFixed(0))
-          currentDownloadInfo.downloadedSize = parseMemorySize(progressEvent.loaded)
-          currentDownloadInfo.downloadSpeed = parseMemorySize(progressEvent.rate) + '/s'
-        },
-        signal: currentDownloadInfo.axiosController.signal
-      }).then(res => {
-        currentDownloadInfo.status = 'finished'
-        currentDownloadInfo.downloadSpeed = '0B/s'
-        const saveFileName = decodeURI(res.headers['content-disposition'].split(';')[1].split('=')[1].replaceAll('"', ''))
-        const blob = new Blob([res.data])
-        saveAs(blob, saveFileName)
-        show('提示', `${fileName} 下载完成`)
-      }).catch((e) => {
-        if (e.message === 'canceled') {
-          currentDownloadInfo.status = 'canceled'
-          show('提示', '下载已取消')
-          return
-        }
-        show('警告', '下载失败')
-      })
+      for (const file of info.files) {
+        await axios({
+          url: '/download',
+          method: 'post',
+          data: {
+            filePath: info.filePath + file
+          },
+          responseType: 'blob',
+          signal: currentDownloadInfo.axiosController.signal,
+          onDownloadProgress: progressEvent => {
+            currentDownloadInfo.fileSize = parseMemorySize(progressEvent.total)
+            currentDownloadInfo.downloadPercent = Number((progressEvent.loaded / progressEvent.total * 100).toFixed(0))
+            currentDownloadInfo.downloadedSize = parseMemorySize(progressEvent.loaded)
+            currentDownloadInfo.downloadSpeed = parseMemorySize(progressEvent.rate) + '/s'
+          }
+        }).then(res => {
+          blobList.push(res.data)
+          if (info.files.length > 1 && blobList.length < info.files.length) {
+            currentDownloadInfo.fileName = `${blobList.length + 1} / ${info.files.length}: ${info.files[blobList.length]}`
+          }
+        })
+      }
+      currentDownloadInfo.fileName = `${info.files.length}个文件压缩中`
+      if (info.files.length === 1) {
+        const blob = new Blob(blobList)
+        saveAs(blob, info.files[0])
+        show('提示', `${info.files[0]} 下载完成`)
+      } else {
+        const zip = new JSZip()
+        blobList.forEach((blob, index) => {
+          zip.file(info.files[index], blob)
+        })
+        zip.generateAsync({ type: 'blob' }).then(content => {
+          saveAs(content, '文件包.zip')
+          currentDownloadInfo.fileName = `${info.files.length}个文件下载完成`
+          show('提示', `文件包共${info.files.length}个文件 下载完成`)
+        })
+      }
+      currentDownloadInfo.status = 'finished'
+      currentDownloadInfo.downloadSpeed = '0B/s'
     },
     cancelDownload (state, index) {
       const currentDownloadInfo = state.downloadInfo.downloadList[index]
